813cff2c2d96889471f04a3132a763d95acb2549
Abbos <abbosvaliev2003@gmail.com>
2023-08-05 21:05:24 +0500

    feat: add functionals javascript and finish project

---
+++ script/script.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var progress = document.querySelector(".progress");
+  var counter = document.querySelector(".counter");
+  var count = 1;
+  var interval = 30 / 8;
+  var timer = null;
+  var payBtn = document.getElementById("payBtn");
+  var inputs = document.querySelectorAll(".input-main");
+
+  function updateProgressBar() {
+    var width = (count / 8) * 100;
+    progress.style.width = width + "%";
+    counter.textContent = count + "/8";
+    
+    if (count === 8) {
+      clearInterval(timer);
+      setTimeout(function() {
+        document.querySelector(".section-2").classList.add("hidden");
+        document.querySelector(".section-3").classList.remove("hidden");
+      }, 3000);
+    }
+    
+    count++;
+  }
+
+  payBtn.disabled = true; // Деактивируем кнопку payBtn при загрузке страницы
+
+  // При изменении значений в инпутах проверяем их заполненность
+  function checkInputs() {
+    var isFilled = true;
+
+    inputs.forEach(function(input) {
+      if (input.value === "") {
+        isFilled = false;
+      }
+    });
+
+    payBtn.disabled = !isFilled; // Активируем или деактивируем кнопку payBtn
+  }
+
+  // Добавляем обработчик события на изменение значений в инпутах
+  inputs.forEach(function(input) {
+    input.addEventListener("input", checkInputs);
+  });
+
+  // Добавляем обработчик события на нажатие кнопки payBtn
+  payBtn.addEventListener("click", function() {
+    if (payBtn.disabled) return; // Если кнопка payBtn деактивирована, выходим из функции
+
+    document.querySelector(".section-1").classList.add("hidden"); // Добавляем класс "hidden" к элементу с классом "section-1"
+    document.querySelector(".section-2").classList.remove("hidden"); // Удаляем класс "hidden" у элемента с классом "section-2"
+    timer = setInterval(updateProgressBar, interval * 1000);
+  });
+});
+
+
+
+// Добавляем 8 белых полосок в блок
+var block = document.getElementById("block");
+for (var i = 0; i < 7; i++) {
+  var divider = document.createElement("div");
+  divider.className = "divider";
+  block.appendChild(divider);
+}
